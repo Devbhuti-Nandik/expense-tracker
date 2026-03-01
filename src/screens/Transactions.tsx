1f08268f2326bd6fee200eb095f68874e933fc43
Devbhuti-Nandika <nandik.bitan@gmail.com>
2026-03-01 18:26:58 +0530

    feat: add group transaction by date logic

--- src/screens/Transactions.tsx
+++ src/screens/Transactions.tsx
@@ -4,15 +4,23 @@ import { StyleSheet, View, Text, Button } from "react-native";
 import { useTransactionStore } from "../store/useTransactionStore";
 import { LightColors } from "../theme/color";
 import { SummaryCard } from "../components/Transactions/SummaryCard";
+import { groupTransactionsByDate } from "../utils/helpers";
 
 const Transactions = () => {
   const navigation = useNavigation();
   const transactions = useTransactionStore((state) => state.transactions);
   useEffect(() => {
     console.log("transactions: ", transactions);
+    const groupedTransactions = groupTransactionsByDate(transactions);
+    console.log(
+      "groupedTransactions: ",
+      JSON.stringify(groupedTransactions, null, 2)
+    );
   }, [transactions]);
   return (
     <View style={styles.transactionsContainer}>
+      {/* Type picker */}
+      {/* TODO: Change the UI of Summary Card completely */}
       <SummaryCard />
     </View>
   );
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,5 +1,10 @@
 import { Transaction } from "../types/transaction";
 
+/**
+ * Sanitize transaction form data
+ * @param transaction - Transaction object
+ * @returns Sanitized transaction object
+ */
 export const sanitizeTransactionFormData = (transaction: Transaction) => {
   const { amount, date, category, description, type } = transaction;
   return {
@@ -13,4 +18,33 @@ export const sanitizeTransactionFormData = (transaction: Transaction) => {
     description: description.trim(),
     type,
   };
+};
+
+/**
+ * Sort transactions by date in descending order
+ * @param transactions - Array of transactions
+ * @returns Array of transactions sorted by date in descending order
+ */
+export const sortTransactionsByDate = (transactions: Transaction[]) => {
+  return [...transactions].sort(
+    (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+  );
+};
+
+/**
+ * Group transactions by date
+ * @param transactions - Array of transactions
+ * @returns Object with date as key and array of transactions as value
+ */
+export const groupTransactionsByDate = (transactions: Transaction[]) => {
+  const sortedTransactions = sortTransactionsByDate(transactions);
+  return sortedTransactions.reduce((acc, transaction) => {
+    //Normalize the date to YYYY-MM-DD format
+    const date = new Date(transaction.date).toISOString().split("T")[0];
+    if (!acc[date]) {
+      acc[date] = [];
+    }
+    acc[date].push(transaction);
+    return acc;
+  }, {} as Record<string, Transaction[]>);
 };
